40ecccdebec61a40d64fb4adb82708a4249ab15c
adrien Clupot <adrien.clupot@gmail.com>
2020-05-27 10:33:41 +0200

    rdy to deploy

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cv',
-    'core',
+    'cv.apps.CvConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
