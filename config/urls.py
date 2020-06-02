8baa94504e398407f7be0ee62ca0d9efe1f3a8f1
adrien Clupot <adrien.clupot@gmail.com>
2020-06-02 11:47:05 +0200

    add cv

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('cv.urls', 'cv'), namespace='cv')),
+    path('cv', include(('cv.urls', 'cv'), namespace='cv')),
 ]
