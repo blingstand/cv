9b12a73db40ad95265fc5f7a5617adab2e12d17d
adrien Clupot <adrien.clupot@gmail.com>
2020-11-03 17:48:23 +0100

    add sass on nav part

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,7 @@
-"""config URL Configuration
+#python libs
+import os
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#django libs
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,3 +9,10 @@ urlpatterns = [
     path('cv/admin/', admin.site.urls),
     path('cv/', include(('cv.urls', 'cv'), namespace='cv')),
 ]
+
+if os.environ["ENV"] == "local":
+	urlpatterns.append(
+		path('', include(('cv.urls', 'cv'), namespace='cv')),
+		)
+
+
